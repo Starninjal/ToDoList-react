23376516e31557aeb79122d18b40f5fdcfe15ab3
Cyp <BR17johnn@gmail.com>
2023-01-31 04:12:03 -0300

    second commit

--- react-to-do-list/src/App.tsx
+++
@@ -1,7 +0,0 @@
-export default function App() {
-  return (
-    <div className="">
-      <h1 className="font-bold m-4 py-5">Haha</h1>
-    </div>
-  )
-}
---
+++ react-to-do-list/src/components/HomeContainer/HomeContainer.ts
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+export const HomeCointainer = styled.div
+  main {
+    background-color: transparent;
+  }
+;
